2239d5d2e4b0fed2471f848320853dac709661ac
Bruno <brunosilvabliu@gmail.com>
2026-09-14 09:01:13 -0300

    arrumando arquivos

--- src/convert_csv/__init__.py
+++ src/convert_csv/__init__.py
@@ -2,7 +2,11 @@ from src.convert_csv.api.routes.post import router as search_csv_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
 
 app.add_middleware(
     CORSMiddleware,
